3252c9637f71c2897a40c41fe86f4ea0747f26f4
MaRuCien <rubio.c.maria@gmail.com>
2022-07-30 01:00:01 +0000

    Actualización de los modelos.

--- src/models.py
+++ src/models.py
@@ -8,37 +8,18 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
-class StarWars(Base):
-    __tablename__ = 'star_wars_database'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
     
-
-class Users(Base):
-    __tablename__ = 'users'
-    id = Column(Integer, primary_key=True)
-    users_email = Column(String(250), nullable=False)
-    users_password = Column(String(100), nullable=False)
-    login_status = Column(String(250), nullable=False)
-    star_wars_id = Column(Integer, ForeignKey('star_wars_database.id'))
-
-
 class User(Base):
-    __tablename__ = 'user'
+    __tablename__ = 'users'
     id = Column(Integer, primary_key=True)
     user_name = Column(String(100), nullable=False)
     user_first_name = Column(String(100), nullable=False)
     user_lastname = Column(String(100), nullable=False)
-    users_id = Column(Integer, ForeignKey('users.id'))
-    favourites_id = Column(Integer, ForeignKey('favourites.id'))
-
-class Favourites(Base):
-    __tablename__ = 'favourites'
-    id = Column(Integer, primary_key=True)
-    user_fav_character_id = Column(Integer, ForeignKey('character.id'))
-    user_fav_planet_id = Column(Integer, ForeignKey('planet.id'))
-    user_fav_vehicle_id = Column(Integer, ForeignKey('vehicle.id'))
+    user_email = Column(String(250), nullable=False)
+    user_password = Column(String(100), nullable=False)
+    login_status = Column(String(250), nullable=False)
+    user_fav = Column(Integer, ForeignKey('user_fav.id'))
+   
 
 class UserFav(Base):
     __tablename__ = 'user_fav'
@@ -53,9 +34,7 @@ class Characters(Base):
     __tablename__ = 'characters'
     id = Column(Integer, primary_key=True)
     character_number = Column(String(250))
-    character_name = Column(String(250))
-    star_wars_id = Column(Integer, ForeignKey('star_wars_database.id'))
-    star_wars_database = relationship(StarWars)
+    character_name = Column(String(250))   
 
 class Character(Base):
     __tablename__ = 'character'
@@ -71,8 +50,7 @@ class Planets(Base):
     id = Column(Integer, primary_key=True)
     planet_number = Column(String(250))
     planet_name = Column(String(250))
-    star_wars_id = Column(Integer, ForeignKey('star_wars_database.id'))
-    star_wars_database = relationship(StarWars)
+    
 
 class Planet(Base):
     __tablename__ = 'planet'
@@ -87,8 +65,7 @@ class Vehicles(Base):
     id = Column(Integer, primary_key=True)
     vehicle_number = Column(String(250))
     vehicle_name = Column(String(250))
-    star_wars_id = Column(Integer, ForeignKey('star_wars_database.id'))
-    star_wars_database = relationship(StarWars)
+    
 
 class Vehicle(Base):
     __tablename__ = 'vehicle'
